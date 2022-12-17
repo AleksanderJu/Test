4ad2abd9a7e1a245542491d11b4e8a6b3a4c3c0e
AleksanderJu <amjalkin@gmail.com>
2022-12-17 19:30:05 +0200

    09_Exceptions

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -19,7 +19,15 @@ public class AppTest {
             System.out.println("Программа перепрыгнула в блок catch!");
             System.out.println("Ошибка! Нельзя делить на ноль!");
         }
+        int a = 10;
+        int b = 0;
 
+        try {
+            int c = a / b;
+        } catch(ArithmeticException e) {
+            System.out.println(e);
+        }
+        System.out.println("Продолжение программы");
 }
 
     }
