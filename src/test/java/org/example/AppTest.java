da0e65fdd934950540a61a14cc706d4177401320
AleksanderJu <amjalkin@gmail.com>
2022-12-17 19:10:27 +0200

    09_Exceptions

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,12 +3,6 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 public class AppTest {
-    //1. Создайте Java класс для тестов в папке test -> java
-    //2. Используя классы Cat и Dog создайте 2 теста
-    //3. Первый тест, используя assert, проверяет что Cat мяукает
-    //4. Второй тест, используя assert, проверяет что Dog гавкает
-    //
-    //5* В pom файле добавьте maven-surefire-plugin и запустите все свои тесты командой mvn test
 
     @Test
     //1. В main или в тесте выполните деление на ноль
@@ -27,14 +21,7 @@ public class AppTest {
         }
 
 }
-    //  public void catTest() {
-      //  Cat cat = new Cat();
-    //    Assertions.assertEquals("Мяу-мяу", cat.sayMeow());
+
     }
 
-  //  @Test
-    //public void dogTest() {
-     //   Dog dog = new Dog();
-      //  Assertions.assertEquals("Гав-гав", dog.sayWoof());
-  //  }
 
